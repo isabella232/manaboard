8297ae0e6694150f6b6b08de8ec690bcf168b3c3
Esteban Ordano <esteban@decentraland.org>
2020-11-16 12:49:43 -0300

    fix: update dcl-ops-lib

--- .ci/index.ts
+++ .ci/index.ts
@@ -1,8 +1,5 @@
-import { env, envTLD } from "dcl-ops-lib/domain";
+import { envTLD } from "dcl-ops-lib/domain";
 import { buildStatic } from "dcl-ops-lib/buildStatic";
-import { globalConfig } from "dcl-ops-lib/values";
-
-const { defaultSecurityGroupName } = globalConfig[env]
 
 async function main() {
   const site = buildStatic({
